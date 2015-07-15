62d7b237af1990cd8f63b35510607ecc618baa34
Harvey Sanders <sanders.harvey@gmail.com>
2015-07-15 10:55:44 -0500

    Make only one marker show at a time

--- js/script.js
+++ js/script.js
@@ -3,11 +3,26 @@ L.mapbox.accessToken = 'pk.eyJ1Ijoic2FuZGVyaGEiLCJhIjoiYTNlMjlhOGEwNDY2YTlmYzNlN
 var map = L.mapbox.map('map', 'mapbox.streets')
     .setView([29.950, -90.066], 13);
 
+var marker;
 
-map.on('click', function(e) {
+var createMarker = function(e) {
+	console.log('in else');
 	var coord = e.latlng;
 	console.log('lat: ' + coord.lat + ' long: ' + coord.lng);
-	L.marker([coord.lat, coord.lng]).addTo(map);
+	marker = new L.Marker(coord);
+	map.addLayer(marker);
+
+	//L.marker([coord.lat, coord.lng]).addTo(map);
+};
+
+map.on('click', function(e) {
+	if (marker) {
+		map.removeLayer(marker);
+		marker = null;
+		createMarker(e);
+	} else {
+		createMarker(e);
+	}
 });
 
 
